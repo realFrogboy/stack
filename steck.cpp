4a72cfc0155fd57864be8a18655cdec7bb8176da
realFrogboy <89666084+realFrogboy@users.noreply.github.com>
2021-10-01 13:58:02 +0500

    Add files via upload

--- steck.cpp
+++ steck.cpp
@@ -1,15 +1,23 @@
+//enum
+//logging
+//
+
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
 
+//#define log_info (...) \
+        fprintf(fd,"%s:%d %s\n", file, line);
+
 enum ERRORS
 {
-    SUCCESS         = 0,
-    STACK_UNDERFLOW = 10101,
-    STACK_OVERFLOW  = 10201,
-    ALLOC_ERROR     = 10301,
-    REALLOC_ERROR   = 10401
+    SUCCESS            = 0,
+    STACK_UNDERFLOW    = 10101,
+    STACK_OVERFLOW     = 10201,
+    ALLOC_ERROR        = 10301,
+    REALLOC_ERROR      = 10401,
+    VOID_STACK         = 10501
 };
 
 const int START_STACK_SIZE = 2;
@@ -19,57 +27,72 @@ struct Stack
 {
     int *data;
     size_t capocity;
-    size_t size;
+    size_t Size;
 };
 
-int stackCtor (Stack* stack);
-int stackPush (Stack* stack, int value);
-int stackPop (Stack* stack, int* x);
-int stackDtor (Stack* stack);
-void printStack(Stack* stack);
-int reallocate (Stack* stack, size_t newSize);
+int stackCtor (Stack* st);
+int stackPush (Stack* st, int value);
+int stackPop (Stack* st, int* x);
+int stackDtor (Stack* st);
+int printStack(Stack* st);
+int reallocate (Stack* st, size_t newSize);
+int stackOK (const Stack* st);
+void stackDump (int error);
+void logInfo (FILE* ptrfile, const char* file, int line, const char* errorCode);
 
 int main ()
 {
+    int error = 0;
+
     struct Stack st;
-    stackCtor (&st);
+    error = stackCtor (&st);
+    if (error != 0) return error;
     printStack (&st);
-    printf ("%d --- %d\n", st.size, st.capocity);
+    printf ("%d --- %d\n", st.Size, st.capocity);
 
-    stackPush (&st, 10);
+    error = stackPush (&st, 10);
     printStack (&st);
-    printf ("%d --- %d\n", st.size, st.capocity);
+    if (error != 0) return error;
+    printf ("%d --- %d\n", st.Size, st.capocity);
 
-    stackPush (&st, 15);
+    error = stackPush (&st, 15);
+    if (error != 0) return error;
     printStack (&st);
-    printf ("%d --- %d\n", st.size, st.capocity);
+    printf ("%d --- %d\n", st.Size, st.capocity);
 
-    stackPush (&st, 20);
+    error = stackPush (&st, 20);
+    if (error != 0) return error;
     printStack (&st);
-    printf ("%d --- %d\n", st.size, st.capocity);
+    printf ("%d --- %d\n", st.Size, st.capocity);
 
     int x = 0;
-    stackPop (&st, &x);
+    error = stackPop (&st, &x);
+    if (error != 0) return error;
     printf ("%d\n", x);
     printStack (&st);
-    printf ("%d --- %d\n", st.size, st.capocity);
+    printf ("%d --- %d\n", st.Size, st.capocity);
 
-    //stackDtor (&st);
+    stackDtor (&st);
 
     return SUCCESS;
 }
 
 
 
-int stackCtor (Stack* stack)
+int stackCtor (Stack* st)
 {
-    assert (stack);
+    int error = stackOK (st);
+    if (error != 0)
+    {
+        stackDump (error);
+        return error;
+    }
 
-    stack->capocity = START_STACK_SIZE;
-    stack->size = 0;
+    st->capocity = START_STACK_SIZE;
+    st->Size = 0;
 
-    stack->data = (int*) calloc (stack->capocity, sizeof (int));
-    if (stack->data == NULL)
+    st->data = (int*) calloc (st->capocity, sizeof (int));
+    if (st->data == NULL)
     {
         printf ("Can't alloc memory\n");
         return ALLOC_ERROR;
@@ -78,67 +101,96 @@ int stackCtor (Stack* stack)
     return SUCCESS;
 }
 
-int stackPush (Stack* stack, int value)
+int stackPush (Stack* st, int value)
 {
-    assert (stack);
-
-    if (stack->size >= stack->capocity)
+    int error = stackOK (st);
+    if (error != 0)
     {
-        reallocate (stack, stack->capocity * COEFFICIENT);
+        stackDump (error);
+        return error;
     }
 
-    *(stack->data + stack->size * sizeof (int)) = value;
-    stack->size++;
+    if (st->Size >= st->capocity)
+    {
+        reallocate (st, st->capocity * COEFFICIENT);
+    }
+
+    *(st->data + st->Size * sizeof (int)) = value;
+    st->Size++;
 
     return SUCCESS;
 }
 
-int stackPop (Stack* stack, int* x)
+int stackPop (Stack* st, int* x)
 {
-    assert (stack);
+    int error = stackOK (st);
+    if (error != 0)
+    {
+        stackDump (error);
+        return error;
+    }
 
-    if (stack->size == 0)
+    if (st->Size == 0)
     {
         printf ("Stack underflow\n");
         return STACK_UNDERFLOW;
     }
 
-    *x = --stack->size;
+    *x = --st->Size;
 
-    if (stack->size <= stack->capocity/COEFFICIENT)
+    if (st->Size <= st->capocity/COEFFICIENT)
     {
-        reallocate (stack, stack->capocity/COEFFICIENT);
+        reallocate (st, st->capocity/COEFFICIENT);
     }
 
 
     return SUCCESS;
 }
 
-int stackDtor (Stack* stack)
+int stackDtor (Stack* st)
 {
-    assert (stack);
+    int error = stackOK (st);
+    if (error != 0)
+    {
+        stackDump (error);
+        return error;
+    }
 
-    //free (stack->data);
-    stack->size = -1;
+    free (st->data);
+    st->Size = -1;
 
     return SUCCESS;
 }
 
-void printStack (Stack* stack)
+int printStack (Stack* st)
 {
-    assert (stack);
-
-    for (int num = 0; num < stack->size; num++)
+    int error = stackOK (st);
+    if (error != 0)
     {
-        printf ("%d\n", *(stack->data + num * sizeof (int)));
+        stackDump (error);
+        return error;
     }
+
+    for (unsigned num = 0; num < st->Size; num++)
+    {
+        printf ("%d\n", *(st->data + num * sizeof (int)));
+    }
+
+    return 0;
 }
 
-int reallocate (Stack* stack, size_t newSize)
+int reallocate (Stack* st, size_t newSize)
 {
-    stack->capocity = newSize;
-    stack->data = (int*) realloc (stack->data, stack->capocity);
-    if (stack->data == NULL)
+    int error = stackOK (st);
+    if (error != 0)
+    {
+        stackDump (error);
+        return error;
+    }
+
+    st->capocity = newSize;
+    st->data = (int*) realloc (st->data, st->capocity);
+    if (st->data == NULL)
         {
             printf ("Can't realloc memory\n");
             return ALLOC_ERROR;
@@ -146,5 +198,39 @@ int reallocate (Stack* stack, size_t newSize)
 
     return SUCCESS;
 }
+
+int stackOK (const Stack* st)
+{
+    if (!st) return VOID_STACK;
+    if (st->Size < 0) return STACK_UNDERFLOW;
+    if (st->capocity < st->Size) return STACK_OVERFLOW;
+
+    return 0;
+}
+
+void stackDump (int error)
+{
+    switch (error)
+    {
+        case STACK_UNDERFLOW:
+            logInfo (stdout, __FILE__, __LINE__, "Stack Underflow\n");
+            break;
+
+        case STACK_OVERFLOW:
+            logInfo (stdout, __FILE__, __LINE__, "Stack Overflow\n");
+            break;
+
+        case VOID_STACK:
+            logInfo (stdout, __FILE__, __LINE__, "Stack if NULL\n");
+
+    }
+}
+
+void logInfo (FILE* ptrfile, const char* file, int line, const char* errorCode)
+{
+    fprintf (ptrfile, "%s:%d %s\n", file, line, errorCode);
+}
+
+
 
 
